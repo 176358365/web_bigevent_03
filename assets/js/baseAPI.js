5cd258ff739123ea2edd8fa41478693cb7be5f1d
liliangping <2733852592@qq.com>
2020-10-10 15:23:42 +0800

    后台主页完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,18 @@ var userURL = 'http://ajax.frontend.itheima.net';
 //控制ajax请求中 的参数
 $.ajaxPrefilter(function (options) {
     options.url = userURL + options.url;
+    //获取有权限得 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    }
+
+    // 权限控制，登录之后才能进去主页面
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+    }
 })
